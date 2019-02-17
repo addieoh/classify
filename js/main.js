ddcd6d41a482f569783515f281a322271ff8aa8f
Jeane Carlos <volbohel@gmail.com>
2019-02-16 23:53:03 -0800

    Initialized Stuff

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.querySelector("#mainButton").addEventListener("click", function() {
+  //Getting the string from the input
+  const userQuery = document.getElementById("userInput").value;
+
+  // Insert your code logic here;
+  console.log("User Query", userQuery);
+});
